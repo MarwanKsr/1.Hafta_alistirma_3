b91399ef2db588f789a0c1b3c3a14bf89763439e
Marwan <Marwan.kaseer1997@gmail.com>
2022-02-21 00:40:17 +0300

    Program is done

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace alistirme_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("kisa kenar: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("uzun kenar: ");
+            int b = int.Parse(Console.ReadLine());
+            int alan = a*b;
+            int cevre = 2*a+2*b;
+            Console.WriteLine("Alan: " + alan);
+            Console.WriteLine("cevre: " + cevre);
+        }
+    }
+}
